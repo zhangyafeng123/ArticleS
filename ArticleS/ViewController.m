c7019415b7bdbc891911392fc2bd6f7d18f0d2b3
张亚峰 <1240334911@qq.com>
2018-10-31 23:35:43 +0800

    测试

--- ArticleS/ViewController.m
+++ ArticleS/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import <AFNetworking.h>
 @interface ViewController ()
+
+
 
 @end
 
@@ -16,7 +18,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+//    [manager GET:@"http://127.0.0.1:8000/articles/getlist" parameters:@{} progress:^(NSProgress * _Nonnull downloadProgress) {
+//
+//    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//
+//        NSLog(@"%@",responseObject);
+//
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//
+//    }];
+    // http://127.0.0.1:8000/user/register
+//    NSDictionary *dic = @{
+//                          @"username":@"xiaodudu",
+//                          @"password":@"zhangyafeng"
+//                          };
+//    [manager POST:@"http://127.0.0.1:8000/user/register" parameters:dic progress:^(NSProgress * _Nonnull uploadProgress) {
+//
+//    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//
+//        NSLog(@"%@", responseObject);
+//
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//        NSLog(@"%@", error);
+//    }];
+    
+    
+    
 }
 
 
